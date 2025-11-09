7f455d094e84b72b653cfa24a9b85a13f571f14c
ShaanDaBest <shaan.mirch@gmail.com>
2025-11-08 19:50:49 -0800

    aerohub.lua

--- aerohub.lua
+++ aerohub.lua
@@ -1,4 +1,5 @@
-https://raw.githubusercontent.com/ShaanDaBest/AeroHub/refs/heads/main/Hub'
+loadstring(HttpGet("https://raw.githubusercontent.com/ShaanDaBest/AeroHub/refs/heads/main/Hub"))()
+
 
 local Window = OrionLib:MakeWindow({Name = "AeroHub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
